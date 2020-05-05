eb0409ef3c4db7ad6d847f533b7c6cee52412c54
no jae gun <njg7194@naver.com>
2020-05-06 05:47:11 +0900

    first commit

---
+++ ServoControl.h
@@ -0,0 +1,50 @@
+#ifndef _ServoControl_
+#define _ServoControl_
+
+#include "arduino.h"
+#include <inttypes.h>
+
+typedef struct PA_8
+{
+    /* data */
+};
+
+typedef struct PA32_
+{
+    /* data */
+};
+
+enum DI
+{
+
+};
+
+enum DO
+{
+
+};
+
+
+enum CONTMODE
+{
+
+};
+
+class ServoControl
+{
+private:
+    /* data */
+public:
+    ServoControl(/* args */);
+    ~ServoControl();
+};
+
+ServoControl::ServoControl(/* args */)
+{
+}
+
+ServoControl::~ServoControl()
+{
+}
+
+#define 
